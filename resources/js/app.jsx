e7ede195f4bac455ac0e66bec8fc583f2af43417
rubab <imran@sslwireless.com>
2024-10-25 14:53:25 +0600

    react running on laravel 11

---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import React from "react"
+import ReactDOM from 'react-dom/client'; 
+
+import HomeReact from './components/HomeReact';
+
+ReactDOM.createRoot(document.getElementById('app')).render(<HomeReact />);
---
+++ resources/js/components/HomeReact.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="row my-5">
+                <div className="col-md-8 mx-auto">
+                    <h1 className="text-danger"> React JS running to laravel 11 with vite</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
